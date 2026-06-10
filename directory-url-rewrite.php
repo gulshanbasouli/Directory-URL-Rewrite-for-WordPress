99ddc4fb596db1060f28c08006d0d08b21a2ff3d
Gulshan Chauhan <32733940+gulshanbasouli@users.noreply.github.com>
2026-06-10 11:58:32 +0530

    Refactor URL rewrite functions for clarity and activation

--- directory-url-rewrite.php
+++ directory-url-rewrite.php
@@ -1,12 +1,37 @@
-// Function File Code
+<?php
+/*
+Plugin Name: Directory URL Rewrite
+Description: Creates SEO-friendly URLs for directory profiles.
+Version: 1.0
+Author: Gulshan Chauhan
+*/
 
-function string_url_rewrite() {
-    global $wp_rewrite;
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+function dur_register_rewrite_rules() {
+
     add_rewrite_tag('%firstName%', '([^&]+)');
     add_rewrite_tag('%lastName%', '([^&]+)');
-    //In the rewrite rule you need to enter page slug and page id
-    add_rewrite_rule('^details/([^/]*)/([^/]*)/?', 'index.php?page_id=177&firstName=$matches[1]&lastName=$matches[2]', 'top');
 
-    $wp_rewrite->flush_rules(true);
+    add_rewrite_rule(
+        '^details/([^/]+)/([^/]+)/?$',
+        'index.php?page_id=177&firstName=$matches[1]&lastName=$matches[2]',
+        'top'
+    );
 }
-add_action('init', 'string_url_rewrite', 10, 0);
+add_action('init', 'dur_register_rewrite_rules');
+
+
+function dur_activate() {
+    dur_register_rewrite_rules();
+    flush_rewrite_rules();
+}
+register_activation_hook(__FILE__, 'dur_activate');
+
+
+function dur_deactivate() {
+    flush_rewrite_rules();
+}
+register_deactivation_hook(__FILE__, 'dur_deactivate');
